25c8097767ff05fae6fbaefb41ead125258bced5
Your Name <you@example.comcjo0012@auburn.edu>
2020-02-22 17:46:28 -0600

    Radio Button added to select between modes. Face detection added to radio button group.

--- socketTest.py
+++ socketTest.py
@@ -12,6 +12,8 @@ from twisted.internet import reactor
 from autobahn.twisted.websocket import WebSocketServerProtocol
 from autobahn.twisted.websocket import WebSocketServerFactory
 
+face_cascade = cv2.CascadeClassifier('C:\Users\CJ\Downloads\data\haarcascades\haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('C:\Users\CJ\Downloads\data\haarcascades\haarcascade_eye.xml')
 
 class MyServerProtocol(WebSocketServerProtocol):
     connections = list()
@@ -72,16 +74,50 @@ class MainApp(QWidget):
     def __init__(self):
         QWidget.__init__(self)
         self.video_size = QSize(1280, 720)
+        self.algorithm_index = 0
         self.setup_ui()
         self.move(300, 300)
 
     def connection(self):
         self.setup_camera()
 
+    def faceDetection(self, frame):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x, y, w, h) in faces:
+            # To draw a rectangle in a face
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2)
+            roi_gray = gray[y:y + h, x:x + w]
+            roi_color = frame[y:y + h, x:x + w]
+
+            # Detects eyes of different sizes in the input image
+            eyes = eye_cascade.detectMultiScale(roi_gray)
+
+            # To draw a rectangle in eyes
+            for (ex, ey, ew, eh) in eyes:
+                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 127, 255), 2)
+        return frame
+
     def getPos(self, event):
         x = event.pos().x()
         y = event.pos().y()
         print(x)
+
+    def btnstate(self, b):
+
+        if b.text() == "None":
+            if b.isChecked() == True:
+                self.algorithm_index = 0
+                self.console.append("No algorithm applied")
+            else:
+                print b.text() + " is deselected"
+
+        if b.text() == "Face Detection":
+            if b.isChecked() == True:
+                self.algorithm_index = 1
+                self.console.append("Face Detection applied")
+            else:
+                print b.text() + " is deselected"
 
     def setup_ui(self):
         """Initialize widgets.
@@ -115,9 +151,25 @@ class MainApp(QWidget):
         self.main_layout.addWidget(self.image_label)
         self.main_layout.addLayout(self.button_menu)
 
+        # Right side UI setup
 
+        self.main_layout1 = QVBoxLayout()
+        self.b1 = QRadioButton("None")
+        self.b1.setChecked(True)
+        self.b1.toggled.connect(lambda: self.btnstate(self.b1))
+        self.main_layout1.addWidget(self.b1)
+        self.b2 = QRadioButton("Face Detection")
+        self.b2.toggled.connect(lambda: self.btnstate(self.b2))
+        self.main_layout1.addWidget(self.b2)
 
-        self.setLayout(self.main_layout)
+        self.console = QTextBrowser()
+        self.main_layout1.addWidget(self.console)
+
+        self.master_layout = QHBoxLayout()
+        self.master_layout.addLayout(self.main_layout)
+        self.master_layout.addLayout(self.main_layout1)
+
+        self.setLayout(self.master_layout)
 
     def setup_camera(self):
         """Initialize camera.
@@ -125,7 +177,7 @@ class MainApp(QWidget):
         self.capture = VideoCap('https://' + self.textbox.text() + ':8080/video')
         # self.capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, self.video_size.width())
         # self.capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, self.video_size.height())
-
+        self.console.append("Video Capture Initialized")
         self.timer = QTimer()
         self.timer.timeout.connect(self.display_video_stream)
         self.timer.start(30)
@@ -134,8 +186,11 @@ class MainApp(QWidget):
         """Read frame from camera and repaint QLabel widget.
         """
         frame = self.capture.read()
-
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        if self.algorithm_index == 0:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        if self.algorithm_index == 1:
+            frame = self.faceDetection(frame)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         image = QImage(frame, frame.shape[1], frame.shape[0],
                        frame.strides[0], QImage.Format_RGB888)
         self.image_label.setPixmap(QPixmap.fromImage(image))
